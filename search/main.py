7dabf6bc4a571d583c53e9261a23996a63d4bcb5
BlindMaks <danbiniek@gmail.com>
2017-11-27 20:55:01 +0100

    dodanie sunday na dwa znaki + refaktor

--- search/main.py
+++ search/main.py
@@ -7,12 +7,12 @@ from search.sunday2char import Sunday2char
 
 getcontext().prec = 50
 
-text = 'bcbcaba2bbcab'
-pattern = '2bbcab'
+text = 'abccbacbacbabcbcbcbabababcbcbabcbcbcbccbcbcbcccbcbbbbaaaabcbacbabcabcbcacbabbcbcbcbbcbccbaacacacacacbabababcbccaaacbacbcbacbacbacbaaaccbbacbacbcabacbacbbcbcbbbcbcbcbaaacacacacaabababcacbbcabcba'
+pattern = 'cacbabababcbccaaa'
 
 start = time.perf_counter()
 force = Force(text, pattern)
-# force = Force(Dictionary.LONG_TEXT.value, Dictionary.MEDIUM_PATTERN.value)
+# force = Force(Dictionary.LONG_TEXT.value, Dictionary.SHORT_PATTERN.value)
 print('Force')
 print(str(force.search()) + ' number of comparison: ' + str(force.numberOfLoops))
 end = time.perf_counter()
@@ -20,7 +20,7 @@ print('took: ' + str(Decimal(end - start)))
 
 start = time.perf_counter()
 sunday = Sunday(text, pattern)
-# sunday = Sunday(Dictionary.LONG_TEXT.value, Dictionary.MEDIUM_PATTERN.value)
+#sunday = Sunday(Dictionary.LONG_TEXT.value, Dictionary.SHORT_PATTERN.value)
 print('Sunday')
 print(str(sunday.search()) + ' number of comparison: ' + str(sunday.numberOfLoops))
 end = time.perf_counter()
@@ -28,7 +28,7 @@ print('took: ' + str(Decimal(end - start)))
 
 start = time.perf_counter()
 sunday2char = Sunday2char(text, pattern)
-# sunday2char = Sunday2char(Dictionary.LONG_TEXT.value, Dictionary.MEDIUM_PATTERN.value)
+#sunday2char = Sunday2char(Dictionary.LONG_TEXT.value, Dictionary.SHORT_PATTERN.value)
 print('Sunday2char')
 print(str(sunday2char.search()) + ' number of comparison: ' + str(sunday2char.numberOfLoops))
 end = time.perf_counter()
--- search/sunday2char.py
+++ search/sunday2char.py
@@ -8,7 +8,7 @@ class Sunday2char(Sunday):
     # TODO coś tu nie działa do sprawdzenia. itr przeskakuje za koniec tekstu i się wypierdala
     def _incrementIterator(self, itr):
         if self.text[itr + self._patternLen:itr + self._patternLen + 2] not in self.pattern:
-            return self._patternLen + 2
+            return self._patternLen + 1
         else:
             return self.pattern[self._patternLen - 1::-1].index(
                 self.text[itr + self._patternLen + 1:itr + self._patternLen - 1:-1]) + 2
