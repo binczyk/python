ed674e3827b288a401f7ab7d4ecc7a659ce62453
Daniel Biniek <danbiniek@gmail.com>
2018-01-12 23:53:15 +0100

    działający algorytm prima

--- graph/Main.py
+++ graph/Main.py
@@ -1,4 +1,23 @@
+import graphviz
+
 from graph.Graph import Graph
+
+dot = graphviz.Digraph()
+
+dot.node('0', '0')
+dot.node('1', '1')
+dot.node('2', '2')
+dot.node('3', '3')
+dot.node('4', '4')
+
+dot.edge('0', '1')
+dot.edge('0', '2')
+dot.edge('1', '4')
+dot.edge('1', '3')
+dot.edge('2', '3')
+dot.edge('3', '4')
+
+dot.render()
 
 graph = Graph(5)
 graph.addEdge(0, 1, 1)
