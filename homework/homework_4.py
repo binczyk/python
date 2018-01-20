b9546ce69b69350c1c64e41b878bcd2fc72fbb19
Daniel Biniek <daniel.biniek@comarch.pl>
2018-01-20 09:24:57 +0100

    poprawki

--- homework/homework_4.py
+++ homework/homework_4.py
@@ -1,51 +1,57 @@
+import math
+import statistics as s
+import time
+from fractions import Fraction as F
+from functools import reduce
+
 print('zad0')
 
 import random
 
 doubleList = [random.uniform(0, 9) for _ in range(40)]
 print(doubleList)
-#
-# print('zad1')
-#
-# print('the mean: ', s.mean(doubleList))
-# # print('the mode: ', s.mode(list))
-# print('the median: ', s.median(doubleList))
-#
-# print('zad2')
-# print(any(2 <= (x ** 2) < 5 for x in doubleList))
-#
-# print('zad3')
-#
-# print(reduce(lambda a, b: a * b, (x * 2 ** (5 * x) for x in doubleList))
-#       - min(v - 4 + (3 * (math.sin(v) ** 2)) if v > 5
-#             else v ** 2 + (5 * math.cos(v) ** 3) for v in doubleList))
-#
-# print('zad4')
-# print(max(list(x - 8 for x in doubleList), key=abs))
-#
-# print('zad5')
-# print(max(list(i * val - i for itr, val in enumerate(doubleList) for i in range(40))))
-#
-# print('zad6')
-# bList = (2, 3, 8, 9)
-# print(sum(a / b for a in doubleList for b in bList))
 
-# print('zad7')
-#
-# start = time.perf_counter()
-# sum(1 / d for d in range(1, 10001))
-# end = time.perf_counter()
-# print('sum(1/d) time: ', end - start)
-#
-# start = time.perf_counter()
-# math.fsum(1 / d for d in range(1, 10001))
-# end = time.perf_counter()
-# print('fsm(1/d) time: ', end - start)
-#
-# start = time.perf_counter()
-# sum(F(1, d) for d in range(1, 10001))
-# end = time.perf_counter()
-# print('sum(F(1,d) time: ', end - start)
+print('zad1')
+
+print('the mean: ', s.mean(doubleList))
+# print('the mode: ', s.mode(list))
+print('the median: ', s.median(doubleList))
+
+print('zad2')
+print(any(2 <= (x ** 2) < 5 for x in doubleList))
+
+print('zad3')
+
+print(reduce(lambda a, b: a * b, (x * 2 ** (5 * x) for x in doubleList))
+      - min(v - 4 + (3 * (math.sin(v) ** 2)) if v > 5
+            else v ** 2 + (5 * math.cos(v) ** 3) for v in doubleList))
+
+print('zad4')
+print(max(list(x - 8 for x in doubleList), key=abs))
+
+print('zad5')
+print(max(list(i * val - i for itr, val in enumerate(doubleList) for i in range(40))))
+
+print('zad6')
+bList = (2, 3, 8, 9)
+print(sum(a / b for a in doubleList for b in bList))
+
+print('zad7')
+
+start = time.perf_counter()
+sum(1 / d for d in range(1, 10001))
+end = time.perf_counter()
+print('sum(1/d) time: ', end - start)
+
+start = time.perf_counter()
+math.fsum(1 / d for d in range(1, 10001))
+end = time.perf_counter()
+print('fsm(1/d) time: ', end - start)
+
+start = time.perf_counter()
+sum(F(1, d) for d in range(1, 10001))
+end = time.perf_counter()
+print('sum(F(1,d) time: ', end - start)
 
 exist = []
 for itr in range(9):
--- homework/homework_5.py
+++ homework/homework_5.py
@@ -1,35 +1,35 @@
 from sympy import *
 
-# init_printing()
-#
-# x, y, a, b, c, d, e, n = symbols('x,y,a,b,c,d,e,n')
-#
-# print('zad1')
-# print(simplify((x ** 2 + x) / ((x * sin(y) ** 2) + (x ** cos(y) ** 2))))
-#
-# print('zad2')
-# print(limit((1 / x) * (log(exp(1) ** x + 1)), x, -oo))
-#
-# print('zad3')
-# print(solveset(x ** 2 + 2 > x ** 3, x, S.Reals) & solveset(sqrt(x + 2) > x, x, S.Reals))
-#
-# print('zad4')
-# print(solveset(a * x ** 3 + b * x ** 2 + c * x + d, x))
-#
-# print('zad5')
-# print(diff(sin(x + y) * cos(x * y), x, y).evalf(30, subs={x: -1, y: 2}))
-#
-# print('zad6')
-# print(plot(3 * x ** 2, diff(x, x), (x, 0, 5)))
-#
-# print('zad7')
-# print(plot(limit((1 + 1 / n) ** (n * x), n, oo), x, (x, -1, 1)))
-#
-# print('zad8')
-# print(integrate(exp(1) ** (-x ** 2), (x, -oo, oo)).evalf(50))
-#
-# print('zad9')
-# print(plot(integrate(exp(1) ** (-x ** 2)), x, (x, -4, 4)))
+init_printing()
+
+x, y, a, b, c, d, e, n = symbols('x,y,a,b,c,d,e,n')
+
+print('zad1')
+print(simplify((x ** 2 + x) / ((x * sin(y) ** 2) + (x ** cos(y) ** 2))))
+
+print('zad2')
+print(limit((1 / x) * (log(exp(1) ** x + 1)), x, -oo))
+
+print('zad3')
+print(solveset(x ** 2 + 2 > x ** 3, x, S.Reals) & solveset(sqrt(x + 2) > x, x, S.Reals))
+
+print('zad4')
+print(solveset(a * x ** 3 + b * x ** 2 + c * x + d, x))
+
+print('zad5')
+print(diff(sin(x + y) * cos(x * y), x, y).evalf(30, subs={x: -1, y: 2}))
+
+print('zad6')
+print(plot(3 * x ** 2, diff(x, x), (x, 0, 5)))
+
+print('zad7')
+print(plot(limit((1 + 1 / n) ** (n * x), n, oo), x, (x, -1, 1)))
+
+print('zad8')
+print(integrate(exp(1) ** (-x ** 2), (x, -oo, oo)).evalf(50))
+
+print('zad9')
+print(plot(integrate(exp(1) ** (-x ** 2)), x, (x, -4, 4)))
 
 print('zad10')
 ax, ay, bx, by, cx, cy = symbols('ax,ay,bx,by,cx,cy')
